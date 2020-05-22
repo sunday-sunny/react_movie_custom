ce0271bc86478ba2c1b450d0fd6a615e0c6b8b5a
sunday-sunny <22mallox@gmail.com>
2020-05-22 12:33:41 +0900

    KOFIC API를 이용하여 movieCd 추출

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,59 @@
 import React from "react";
 import axios from "axios";
+import moment from "moment";
+import MovieDetail from "./MovieDetail";
 
 class Home extends React.Component {
+  // KOFIC API
+  KOFIC = {
+    API_URL:
+      "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json",
+    KEY: "c74d1e3ccf220bb37d0dea10acafcfa5",
+  };
+
   state = {
     isLoading: true,
     movies: [],
   };
 
+  getMovies = async () => {
+    // KOFIC BOXOFFICE API
+    const yesterday = moment().subtract(1, "d").format("YYYYMMDD");
+    const {
+      data: {
+        boxOfficeResult: { dailyBoxOfficeList },
+      },
+    } = await axios.get(this.KOFIC.API_URL, {
+      params: {
+        key: this.KOFIC.KEY,
+        targetDt: yesterday,
+        multiMovieYn: "N",
+      },
+    });
+    this.setState({ movies: dailyBoxOfficeList, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
-    const { isLoading } = this.state.isLoading;
-    return { isLoading } ? <h1>true</h1> : <h1>false</h1>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div className="container">
+        {isLoading ? (
+          <div className="container__loader">
+            <span className="loader_text">isLoading...</span>
+          </div>
+        ) : (
+          <div className="container__movies">
+            {movies.map((movie) => (
+              <MovieDetail key={movie.movieCd} movieCd={movie.movieCd} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
